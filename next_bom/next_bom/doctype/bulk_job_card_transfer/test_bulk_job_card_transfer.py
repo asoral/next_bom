26c4e8881f5427e2a8cdb6076d9577732e7275c3
Satish <sshingane@dexciss.com>
2025-04-29 15:39:38 +0530

    add job card functionality

---
+++ next_bom/next_bom/doctype/bulk_job_card_transfer/test_bulk_job_card_transfer.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Chiranjeevi Subburaj and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBulkJobCardTransfer(FrappeTestCase):
+	pass

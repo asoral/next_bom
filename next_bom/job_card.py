6d3f68b43d3efd71be0a815708d3997b247517ec
Satish <sshingane@dexciss.com>
2025-05-02 12:06:15 +0530

    add job card customisation

--- next_bom/job_card.py
+++ next_bom/job_card.py
@@ -63,6 +63,7 @@ def child_table_append(data, doc):
         qty = i.get("qty")
         job_card_id = i.get("job_card")
         job_name = i.get("job_name")
+        date=i.get("date")
 
         if qty:
             total_qty_trans += qty
@@ -81,7 +82,7 @@ def child_table_append(data, doc):
 
             job_card.append("custom_received_qty_", {
                 "transferred_from_job_card_id": job_name,
-                "date_and_time": now(),
+                "date_and_time": date,
                 "received_qty": qty
             })
 
